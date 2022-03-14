69b384f51bb70798c21e516f6c72a19dfa89a668
Petrov Ilya <dnamix1@gmail.com>
2022-03-14 14:51:19 +0300

    layered architecture walking skeleton implementation

---
+++ WebApi/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] // get all activities
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")] // get single activity by ID
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+    }
+}
